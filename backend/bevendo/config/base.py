ca8deab784e2b607305b2dd7be9ac936db8096f3
Bryan Hadro <bryan@merchbar.com>
2023-01-31 15:45:24 -0500

    turns off token blacklist for now

--- backend/bevendo/config/base.py
+++ backend/bevendo/config/base.py
@@ -140,7 +140,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }

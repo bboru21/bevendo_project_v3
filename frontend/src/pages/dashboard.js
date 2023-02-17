ed0815b98c190180d64084829f9348f54581c639
Bryan Hadro <bryan@merchbar.com>
2023-02-17 10:16:48 -0500

    adds getServerSideProps to dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,8 +1,10 @@
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import Layout from '../hocs/Layout';
+import cookie from 'cookie';
+import { API_URL } from '../config/index';
 
-const Dashboard = () => {
+const Dashboard = ({ error, feasts }) => {
    
     const router = useRouter();
 
@@ -27,10 +29,87 @@ const Dashboard = () => {
                     <p className='fs-4 mt-3'>
                         Welcome to Bevendo{user === null ? '' : `, ${user.first_name}`}!
                     </p>
+                    {error && (
+                        <p className='fs-4 mt-3'>
+                            {error}
+                        </p>
+                    )}
                 </div>
+                {feasts && (
+                    <div className='container-fluid py-3'>
+                        <h2 className='display-7 fw-bold'>
+                            This Weeks Feasts &amp; Cocktails
+                        </h2>
+                        <ul>
+                            {feasts.map(feast => (
+                                <li>
+                                    <p className="mb-0">{feast.name} - {feast.date}</p>
+                                    {feast.cocktails && (
+                                        <ul>
+                                            {feast.cocktails.map(cocktail => (
+                                                <li>{cocktail.name}</li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
            </div>
         </Layout>
     );
 };
+
+
+export async function getServerSideProps({ req }) {
+
+    const cookies = cookie.parse(req.headers.cookie ?? '');
+    const access = cookies.access ?? false;
+
+    // this should never happen, but just in case
+    if (access === false) {
+        return {
+            props: {
+                error: 'User unauthorized to load page data',
+            }
+        };
+    }
+
+    try {
+        const res = await fetch(`${API_URL}/api/v1/pages/dashboard`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${access}`,
+            },
+        });
+    
+        if (res.status === 200) {
+            const data = await res.json();
+
+            return {
+                props: {
+                    feasts: data.feasts,
+                }
+            };
+
+        } else {
+            console.error(`API request returned status ${res.status}`);
+            return {
+                props: {
+                    error: 'Something went wrong loading page data',
+                }
+            };
+        }
+    } catch(error) {
+        console.error(error);
+        return {
+            props: {
+                error: 'Something went wrong loading page data',
+            }
+        }
+    }
+}
 
 export default Dashboard;

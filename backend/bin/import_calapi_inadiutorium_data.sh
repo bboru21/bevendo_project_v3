f4f4fd3bf44d6e696a850d101ed762c6f2942cd1
Bryan Hadro <bryan@merchbar.com>
2023-03-06 09:43:23 -0500

    add cryptography and pyopenssl packages to fix scrapy openssl exception

--- backend/bin/import_calapi_inadiutorium_data.sh
+++ backend/bin/import_calapi_inadiutorium_data.sh
@@ -7,4 +7,4 @@ VE_PATH="/home/webmaster/bevendo_project/backend/venv"
 
 cd /home/webmaster/bevendo_project/backend
 
-$VE_PATH/bin/python3 manage.py import_calapi_inadiutorium_data --settings bevendo.config.production
+$VE_PATH/bin/python3 manage.py import_calapi_inadiutorium_data --settings bevendo.config.prod
--- backend/bin/send_weekly_email.sh
+++ backend/bin/send_weekly_email.sh
@@ -7,4 +7,4 @@ VE_PATH="/home/webmaster/bevendo_project/backend/venv"
 
 cd /home/webmaster/bevendo_project/backend
 
-$VE_PATH/bin/python3 manage.py send_weekly_email --settings bevendo.config.production
+$VE_PATH/bin/python3 manage.py send_weekly_email --settings bevendo.config.prod

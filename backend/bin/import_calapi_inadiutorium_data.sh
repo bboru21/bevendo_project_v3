36ed53cf217979c05d00fb1b14e3058775202a6a
Bryan Hadro <bryan@merchbar.com>
2023-03-06 09:18:37 -0500

    update directory structure

--- backend/bin/import_calapi_inadiutorium_data.sh
+++ backend/bin/import_calapi_inadiutorium_data.sh
@@ -3,8 +3,8 @@
 echo 'import_calapi_inadiutorium_data'
 MAILTO=""
 
-VE_PATH="/home/webmaster/bevendo_project/venv"
+VE_PATH="/home/webmaster/bevendo_project/backend/venv"
 
-cd /home/webmaster/bevendo_project
+cd /home/webmaster/bevendo_project/backend
 
-$VE_PATH/bin/python3 bevendo/manage.py import_calapi_inadiutorium_data --settings bevendo.settings.production
+$VE_PATH/bin/python3 manage.py import_calapi_inadiutorium_data --settings bevendo.config.production
--- backend/bin/send_weekly_email.sh
+++ backend/bin/send_weekly_email.sh
@@ -3,8 +3,8 @@
 echo 'send_weekly_email'
 MAILTO=""
 
-VE_PATH="/home/webmaster/bevendo_project/venv"
+VE_PATH="/home/webmaster/bevendo_project/backend/venv"
 
-cd /home/webmaster/bevendo_project
+cd /home/webmaster/bevendo_project/backend
 
-$VE_PATH/bin/python3 bevendo/manage.py send_weekly_email --settings bevendo.settings.production
+$VE_PATH/bin/python3 manage.py send_weekly_email --settings bevendo.config.production

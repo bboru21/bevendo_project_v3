8f7361d3b0f24ccf51d8817d53e1e1f12817d70a
Bryan Hadro <bryan@merchbar.com>
2023-03-22 09:35:47 -0400

    add node environment banner for lower environments

--- frontend/src/hocs/Layout.js
+++ frontend/src/hocs/Layout.js
@@ -19,6 +19,11 @@ const Layout = ({ title, content, children }) => {
                 <title>{title}</title>
                 <meta name='description' content={content} />
             </Head>
+            {process.env.NODE_ENV !== 'production' && (
+                <div style={{ width: '100%', backgroundColor: 'red', color: 'white', textAlign: 'center'}}>
+                    Node Environment: {process.env.NODE_ENV}
+                </div>
+            )}
             <Navbar />
             <div className='container mt-5'>{children}</div>
         </>

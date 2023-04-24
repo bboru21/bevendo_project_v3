d3c8398e7f65dd7102acfc5f0caaeec8b0124bfb
Bryan Hadro <bryan@merchbar.com>
2023-04-24 10:44:19 -0400

    adds ExternalLink noreferrer

--- frontend/src/components/ExternalLink.js
+++ frontend/src/components/ExternalLink.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
 
 const ExternalLink = ({ href, children }) => (
-  <a href={href} target="_blank">
+  <a href={href} target="_blank" rel="noreferrer">
     <>
       {children}
       <FontAwesomeIcon icon={faExternalLink} className="ms-1" size="1x" />

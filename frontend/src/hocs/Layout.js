f7a81775b3eab1ed005f4488aeffa4705056a2cc
Bryan Hadro <bryan@merchbar.com>
2023-04-24 10:07:02 -0400

    adds global footer and contact link

--- frontend/src/hocs/Layout.js
+++ frontend/src/hocs/Layout.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 import Navbar from '../components/Navbar';
 
 const Layout = ({ title, content, children }) => {
-    
+
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -19,13 +19,19 @@ const Layout = ({ title, content, children }) => {
                 <title>{title}</title>
                 <meta name='description' content={content} />
             </Head>
-            {process.env.NODE_ENV !== 'production' && (
-                <div style={{ width: '100%', backgroundColor: 'red', color: 'white', textAlign: 'center'}}>
-                    Node Environment: {process.env.NODE_ENV}
-                </div>
-            )}
-            <Navbar />
-            <div className='container mt-5'>{children}</div>
+
+            <div id="global-container">
+                {process.env.NODE_ENV !== 'production' && (
+                    <div style={{ width: '100%', backgroundColor: 'red', color: 'white', textAlign: 'center'}}>
+                        Node Environment: {process.env.NODE_ENV}
+                    </div>
+                )}
+                <Navbar />
+                <div className='container mt-5'>{children}</div>
+                <footer className="bg-light p-3">
+                    <a href="mailto:bryan.e.hadro@gmail.com">Contact</a>
+                </footer>
+            </div>
         </>
     );
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../hocs/Layout';
+import Link from 'next/link';
 
 const homePage = () => (
   <Layout
@@ -9,8 +10,9 @@ const homePage = () => (
       <div className='container-fluid py-3'>
         <h1 className='display-5 fw-bold'>Home Page</h1>
         <p className='fs-4 mt-3'>
-          Welcome to Bevendo, a companion app to <i>Drinking with the Saints</i>!
+          Welcome to Bevendo, a cocktail app project.
         </p>
+        <p className="text-muted">Please <Link href="/login">login</Link> to continue.</p>
       </div>
     </div>
   </Layout>

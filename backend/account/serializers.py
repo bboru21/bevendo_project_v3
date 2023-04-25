bb10b44bd0aa26731cbe640bf6378cd8c20a75aa
Bryan Hadro <bryan@merchbar.com>
2023-04-25 17:24:17 -0400

    adds user favorites state check

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+
+    # TODO should invest the time to use related_name favorites to expose cocktail id
+    favorites = serializers.SerializerMethodField()
+
+    def get_favorites(self, obj):
+        return obj.favorites.values_list("cocktail__pk", flat=True)
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'favorites')

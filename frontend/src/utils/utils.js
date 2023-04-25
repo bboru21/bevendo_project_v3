3005dab831ca98ba90b69ad2b1ee474dc2475460
Bryan Hadro <bryan@merchbar.com>
2023-04-25 09:16:10 -0400

    fixes date issue

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,3 @@
-/* TODO probably remove this if we implement moment.js */
 export const nthNumber = (number) => {
   if (number > 3 && number < 21) return "th";
   switch (number % 10) {
@@ -13,12 +12,14 @@ export const nthNumber = (number) => {
   }
 };
 
-/* TODO probably remove this if we implement moment.js */
 export const displayDate = (date) => {
-  const d = new Date(date);
+
+  const d = new Date(`${date}T00:00:00`);
+
   const weekday = d.toLocaleDateString('en-us', { weekday: 'long' });
   const month = d.toLocaleDateString('en-us', { month: 'long' });
   const day = d.toLocaleDateString('en-us', { day: 'numeric' });
+
 
   return `${weekday}, ${month} ${day}${nthNumber(day)}`;
 }

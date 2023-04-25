96cc28e1aa9a4a09800e2b91d9e690ee6120c4e1
Bryan Hadro <bryan@merchbar.com>
2023-04-25 08:55:19 -0400

    adds isAuthenticated check around homepage login link

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,21 +1,29 @@
+import { useSelector } from 'react-redux';
 import Layout from '../hocs/Layout';
 import Link from 'next/link';
 
-const homePage = () => (
-  <Layout
-    title='Bevendo | Home'
-    content='Home page for Bevendo, a companion app to Drinking with the Saints'
-  >
-    <div className='p-5 bg-light rounded-3'>
-      <div className='container-fluid py-3'>
-        <h1 className='display-5 fw-bold'>Home Page</h1>
-        <p className='fs-4 mt-3'>
-          Welcome to Bevendo, a cocktail app project.
-        </p>
-        <p className="text-muted">Please <Link href="/login">login</Link> to continue.</p>
+const homePage = () => {
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <Layout
+      title='Bevendo | Home'
+      content='Home page for Bevendo, a companion app to Drinking with the Saints'
+    >
+      <div className='p-5 bg-light rounded-3'>
+        <div className='container-fluid py-3'>
+          <h1 className='display-5 fw-bold'>Home Page</h1>
+          <p className='fs-4 mt-3'>
+            Welcome to Bevendo, a cocktail app project.
+          </p>
+          {!isAuthenticated && (
+            <p className="text-muted">Please <Link href="/login">login</Link> to continue.</p>
+          )}
+        </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default homePage;

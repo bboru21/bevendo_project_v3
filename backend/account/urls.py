54cd3fe825fc2aebacd99d23701ecb512410b00b
Bryan Hadro <bryan@merchbar.com>
2023-05-25 09:47:11 -0400

    adds change password view

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from .views import (
     index,
-    RegisterView, 
+    RegisterView,
     LoadUserView,
+    ChangePasswordView,
 )
 
 urlpatterns = [
     path('', index),
     path('register', RegisterView.as_view()),
     path('user', LoadUserView.as_view()),
+    path('change-password', ChangePasswordView.as_view())
 ]

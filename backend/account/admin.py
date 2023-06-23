71dba3f1943933718b497f85a1c67a3be2ac986f
Bryan Hadro <bryan@merchbar.com>
2023-06-23 09:35:11 -0400

    adds CustomUserAdmin

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'is_staff', 'last_login')
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)

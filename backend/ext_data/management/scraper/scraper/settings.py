e03c054acc72efa71d41619fcc4bcce4732b685e
Bryan Hadro <bryan@merchbar.com>
2024-11-04 09:35:32 -0500

    adds user agent string to scrapy settings

--- backend/ext_data/management/scraper/scraper/settings.py
+++ backend/ext_data/management/scraper/scraper/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'scraper (+http://bevendo.online)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True

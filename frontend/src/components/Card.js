4fbeb766c266cf601f676f33ca79d9c773119201
Bryan Hadro <bryan@merchbar.com>
2025-02-27 16:31:59 -0500

    adds FavoriteButton to cocktails page

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import classNames from 'classnames';
 import css from './Card.module.scss';
 import Image from 'next/image';
+
 const Container = ({ href, className: classNameProp, children }) => {
     if (href) {
         return (
@@ -14,7 +15,7 @@ const Container = ({ href, className: classNameProp, children }) => {
     }
     return <div className={classNames(css.container, classNameProp)}>{children}</div>;
 };
-const Card = ({ href, title, image, zoom=false, className: classNameProp}) => (
+const Card = ({ href, title, image, zoom=false, className: classNameProp, button}) => (
     <Container 
         href={href} 
         className={classNames(classNameProp, {
@@ -23,11 +24,12 @@ const Card = ({ href, title, image, zoom=false, className: classNameProp}) => (
     >
         <div className={css.innerContainer}>
             <div className={css.content}>
-                <h3>{title}</h3>
-                
+                <h3>{title}</h3>   
             </div>
 
             {image && <Image src={`${process.env.NEXT_PUBLIC_API_URL}${image}`} alt={title} priority={false} layout="fill" className={css.image} />}
+
+            {button && <div className={classNames(css.button, 'm-2')}>{button}</div>}
 
         </div>
     </Container>     

64a83aacab666d13607c1e51177140ae853c39d3
Bryan Hadro <bryan@merchbar.com>
2025-02-27 14:51:46 -0500

    removes restProps

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,25 +2,24 @@ import Link from 'next/link';
 import classNames from 'classnames';
 import css from './Card.module.scss';
 import Image from 'next/image';
-const Container = ({ href, className: classNameProp, children, ...restProps }) => {
+const Container = ({ href, className: classNameProp, children }) => {
     if (href) {
         return (
-            <Link href={href} legacyBehavior {...restProps}>
+            <Link href={href} legacyBehavior>
                 <a className={classNames(css.container, css.link, classNameProp)}>
                     {children}
                 </a>    
             </Link>
         );
     }
-    return <div className={classNames(css.container, classNameProp)} {...restProps}>{children}</div>;
+    return <div className={classNames(css.container, classNameProp)}>{children}</div>;
 };
-const Card = ({ href, title, image, zoom=false, className: classNameProp, ...restProps}) => (
+const Card = ({ href, title, image, zoom=false, className: classNameProp}) => (
     <Container 
         href={href} 
         className={classNames(classNameProp, {
             [css.zoom]: zoom,
         })}
-        {...restProps}
     >
         <div className={css.innerContainer}>
             <div className={css.content}>
